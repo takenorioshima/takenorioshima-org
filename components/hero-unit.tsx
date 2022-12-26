6bc33caa4540d42f0107e2b61a0adf218be6a4aa
Takenori Oshima <takenorioshima@gmail.com>
2022-12-26 11:13:27 +0900

    update: Add hero unit animation

--- components/hero-unit.tsx
+++ components/hero-unit.tsx
@@ -1,32 +1,56 @@
-export default function HeroUnit(){
-    return(
-        <>
-            <div id="hero-unit" className="hero-unit">
+import React, { useEffect } from "react";
 
-                <div className="">
-                    <div className="flip-titles-container">
-                        <p>大島武宜は</p>
-                        <div className="flip-titles">
-                            <div className="flip-title">コンポーザー</div>
-                            <div className="flip-title">ギタリスト</div>
-                            <div className="flip-title">デザイナー</div>
-                            <div className="flip-title">音楽家</div>
-                            <div className="flip-title">プログラマ</div>
-                            <div className="flip-title">自由工作員</div>
-                            <div className="flip-title">装丁家</div>
-                            <div className="flip-title">歌うたい</div>
-                            <div className="flip-title">電子工作家</div>
-                        </div>
-                        <p>です。</p>
-                    </div>
-                </div>
+export default function HeroUnit() {
+  useEffect(() => {
+    const titles = document.querySelectorAll(".flip-titles > div");
+    let index = 0;
+    let index_inactive = 0;
+    setInterval(addActiveClass, 1000);
+    function addActiveClass() {
+      titles.forEach((element) => {
+        element.classList.remove("active", "inactive");
+      });
+      if (index === 0) {
+        index_inactive = titles.length - 1;
+      } else {
+        index_inactive = index - 1;
+      }
+      titles[index].classList.add("active");
+      titles[index_inactive].classList.add("inactive");
+      if (index < titles.length - 1) {
+        index++;
+      } else {
+        index = 0;
+      }
+    }
+  }, []);
 
-                <div className="hero-unit-scroll">
-                    <small>Scroll</small>
-                    <i className="bi-chevron-down"></i>
-                </div>
-
+  return (
+    <>
+      <div id="hero-unit" className="hero-unit">
+        <div className="relative flex items-center justify-center w-full h-full">
+          <div className="flip-titles-container">
+            <p>大島武宜は</p>
+            <div className="flip-titles">
+              <div className="flip-title">コンポーザー</div>
+              <div className="flip-title">ギタリスト</div>
+              <div className="flip-title">ＷＥＢデザイナー</div>
+              <div className="flip-title">音楽家</div>
+              <div className="flip-title">プログラマ</div>
+              <div className="flip-title">自由工作員</div>
+              <div className="flip-title">ブックデザイナー</div>
+              <div className="flip-title">歌うたい</div>
+              <div className="flip-title">電子工作家</div>
             </div>
-        </>
-    )
+            <p>です。</p>
+          </div>
+        </div>
+
+        <div className="hero-unit-scroll">
+          <small>Scroll</small>
+          <i className="bi-chevron-down"></i>
+        </div>
+      </div>
+    </>
+  );
 }
