a7c07f554a51ba79704f772ef08aa2bca90d0990
Takenori Oshima <takenorioshima@gmail.com>
2022-12-28 13:43:27 +0900

    update: Add three.js hero unit

--- components/hero-unit.tsx
+++ components/hero-unit.tsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect, useRef } from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
+import { OrbitControls, OrthographicCamera } from "@react-three/drei";
+import Model from "./model";
 
 export default function HeroUnit() {
   useEffect(() => {
@@ -45,6 +48,20 @@ export default function HeroUnit() {
             </div>
             <p>です。</p>
           </div>
+        </div>
+
+        <div className="absolute top-0 left-0 w-full h-full">
+          <Canvas
+            orthographic={true}
+            camera={{ zoom: 100, position: [10, 5, 0] }}
+          >
+            <OrbitControls autoRotate={true} enableZoom={false} />
+            <ambientLight intensity={0.1} />
+            <directionalLight color="white" position={[0, 0, 5]} />
+            <Suspense fallback={null}>
+              <Model scale={10} />
+            </Suspense>
+          </Canvas>
         </div>
 
         <div className="hero-unit-scroll">
