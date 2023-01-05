3c4ba0fa02fe75f2882022151a51aef95b9e1662
Takenori Oshima <takenorioshima@gmail.com>
2023-01-05 14:42:10 +0900

    update: Add rehype-prism-plus

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -4,12 +4,14 @@ import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import rehypeSlug from "rehype-slug";
 import rehypeRaw from "rehype-raw";
+import rehypePrism from "rehype-prism-plus";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
     .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypePrism)
     .use(rehypeRaw)
     .use(rehypeStringify)
     .use(rehypeSlug)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from "next/app";
 import { useEffect } from "react";
 import Script from "next/script";
 import "../styles/index.css";
+import "../styles/prism.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const handleScroll = () => {
