d6c54807d2ba7231d4a775c66474301ff0d2a2dc
Takenori Oshima <takenorioshima@gmail.com>
2023-01-06 15:22:55 +0900

    refactor: Split component

---
+++ src/components/sidebar-toc.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import * as tocbot from "tocbot";
+
+const SidebarToc = () => {
+  useEffect(() => {
+    tocbot.init({
+      // Where to render the table of contents.
+      tocSelector: ".js-toc",
+      // Where to grab the headings to build the table of contents.
+      contentSelector: ".js-toc-content",
+      // Which headings to grab inside of the contentSelector element.
+      headingSelector: "h1, h2, h3",
+      // For headings inside relative or absolute positioned containers within content.
+      hasInnerContainers: true,
+      headingsOffset: 60,
+      scrollSmoothOffset: -60,
+    });
+    return () => tocbot.destroy();
+  }, []);
+
+  return (
+    <>
+      <div className="sticky top-[80px]">
+        <h4 className="text-gray-700 font-semibold text-sm">目次</h4>
+        <div className="js-toc"></div>
+      </div>
+    </>
+  );
+}
+
+export default SidebarToc;
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,26 +2,10 @@ import React, { useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import SidebarToc from "./sidebar-toc";
 import { AUTHOR_PROFILES } from "../lib/constants";
-import * as tocbot from "tocbot";
 
 const Sidebar = () => {
-
-  useEffect(() => {
-    tocbot.init({
-      // Where to render the table of contents.
-      tocSelector: ".js-toc",
-      // Where to grab the headings to build the table of contents.
-      contentSelector: ".js-toc-content",
-      // Which headings to grab inside of the contentSelector element.
-      headingSelector: "h1, h2, h3",
-      // For headings inside relative or absolute positioned containers within content.
-      hasInnerContainers: true,
-      headingsOffset: 60,
-      scrollSmoothOffset: -60,
-    });
-    return () => tocbot.destroy();
-  }, []);
 
   const links = AUTHOR_PROFILES.links;
   const linkList = [];
@@ -59,10 +43,7 @@ const Sidebar = () => {
           </div>
         </div>
       </div>
-      <div className="sticky top-[80px]">
-        <h4 className="text-gray-700 font-semibold text-sm">目次</h4>
-        <div className="js-toc"></div>
-      </div>
+      <SidebarToc />
     </>
   );
 };
