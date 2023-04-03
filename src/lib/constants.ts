c174caac845142e59db3fe86b5a833acb00ae13b
Takenori Oshima <takenorioshima@gmail.com>
2023-04-03 16:13:29 +0900

    update: Replace og:image

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,4 @@
-export const HOME_OG_IMAGE_URL =
-  "https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
+export const HOME_OG_IMAGE_URL = "/images/ogp.png";
 export const SITE_NAME = "大島 武宜 | Takenori Oshima";
 export const AUTHOR_PROFILES = {
   name: {
