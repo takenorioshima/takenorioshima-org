d447c931e723cb15f3dcec31e4f3589b192e3b6d
Takenori Oshima <takenorioshima@gmail.com>
2023-04-11 13:02:53 +0900

    update: 404 を getStaticProps で制御する

--- src/pages/sitemap.tsx
+++ src/pages/sitemap.tsx
@@ -3,14 +3,10 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Sitemap() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace("/404");
-  }, []);
   return null;
 }
 
 export const getStaticProps = async () => {
   await generateSitemapXml();
-  return { props: {} };
+  return { notFound: true };
 };
