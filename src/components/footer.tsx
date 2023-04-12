aac9f74f56e0b5b7dc1c18ff5a7667bfd290a665
Takenori Oshima <takenorioshima@gmail.com>
2023-04-12 16:38:51 +0900

    fix: Fix typo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="py-10 px-5 border-t bg-gray-50">
-      <div class="flex justify-between">
+      <div className="flex justify-between">
         <Link href="/policies" className="text-sm text-gray-500">
           <i className="bi bi-shield-lock-fill"></i> Privacy Policy
         </Link>
