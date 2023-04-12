9b3fbdf11a3f2c44c228a215bcac35a88efe9579
Takenori Oshima <takenorioshima@gmail.com>
2023-04-12 16:02:13 +0900

    update: Add link to policies

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="p-10 border-t bg-gray-50">
-      <p className="text-center text-sm text-gray-500">&copy; takenorioshima</p>
+    <footer className="py-10 px-5 border-t bg-gray-50">
+      <div class="flex justify-between">
+        <Link href="/policies" className="text-sm text-gray-500">
+          <i className="bi bi-shield-lock-fill"></i> Privacy Policy
+        </Link>
+        <p className="text-sm text-gray-500">&copy; takenorioshima</p>
+      </div>
     </footer>
   );
 };
