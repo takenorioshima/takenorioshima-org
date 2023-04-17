52a64dd7405f23d2280a0c04d3d19275e8a45f7d
Takenori Oshima <takenorioshima@gmail.com>
2023-04-17 13:40:32 +0900

    update: Wrap with container

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
+import Container from "./container";
 
 const Footer = () => {
   return (
-    <footer className="py-10 px-5 border-t bg-gray-50">
-      <div className="flex justify-between">
-        <Link href="/policies" className="text-sm text-gray-500">
-          <i className="bi bi-shield-lock-fill"></i> Privacy Policy
-        </Link>
-        <p className="text-sm text-gray-500">&copy; takenorioshima</p>
-      </div>
+    <footer className="pb-1 px-5 border-t bg-gray-50">
+      <Container>
+        <div className="flex justify-between">
+          <Link href="/policies" className="text-sm text-gray-500">
+            <i className="bi bi-shield-lock-fill"></i> Privacy Policy
+          </Link>
+          <p className="text-sm text-gray-500">&copy; takenorioshima</p>
+        </div>
+      </Container>
     </footer>
   );
 };
