bd04ebf18c2ce8d4467a5fde64ac8b388336d5cd
Takenori Oshima <takenorioshima@gmail.com>
2023-05-09 16:54:25 +0900

    fix: Tweak href

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="pb-1 px-5 border-t bg-gray-50">
       <Container>
         <div className="flex justify-between">
-          <Link href="/policies" className="text-sm text-gray-500">
+          <Link href="/policies/" className="text-sm text-gray-500">
             <i className="bi bi-shield-lock-fill"></i> Privacy Policy
           </Link>
           <p className="text-sm text-gray-500">&copy; takenorioshima</p>
