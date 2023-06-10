2abb07dc6cbc96d4fc8c3ea072621d1b426c46f9
Takenori Oshima <takenorioshima@gmail.com>
2023-06-10 12:14:32 +0900

    update: Add page title to index of posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,30 +4,29 @@ import Layout from "../../components/layouts/posts";
 import { getAllPosts } from "../../lib/api";
 import { NextSeo } from "next-seo";
 import Post from "../../interfaces/post";
-import Footer from "../../components/footer";
+import { SITE_NAME } from "../../lib/constants";
 
 type Props = {
   posts: Post[];
 };
 
 export default function Posts({ posts }: Props) {
+  const title = `最近の投稿 - ${SITE_NAME}`;
   return (
-    <>
-      <Layout>
-        <NextSeo />
-        <Container>
-          <h2 className="text-2xl font-bold tracking-tighter md:pr-8 py-5">
-            <i className="bi bi-journals"></i> 最近の投稿
-          </h2>
-          <MoreStories posts={posts} />
-        </Container>
-      </Layout>
-    </>
+    <Layout>
+      <NextSeo title={title} />
+      <Container>
+        <h2 className="text-2xl font-bold tracking-tighter md:pr-8 py-5">
+          <i className="bi bi-journals"></i> 最近の投稿
+        </h2>
+        <MoreStories posts={posts} />
+      </Container>
+    </Layout>
   );
 }
 
 export const getStaticProps = async () => {
-  const posts = getAllPosts(["title", "date", "slug", "coverImage", "excerpt"]);
+  const posts = getAllPosts(["title", "date", "slug", "coverImage", "excerpt", "content"]);
 
   return {
     props: { posts },
