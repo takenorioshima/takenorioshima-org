c4fea0b62a2f7ad531613d2f4e7e70c5bcccc98e
Takenori Oshima <takenorioshima@gmail.com>
2023-11-06 10:37:01 +0900

    update: Replace twitter icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,9 +16,9 @@ const Header = () => {
   const links = AUTHOR_PROFILES.links;
   const linkList = [];
   for (const [key, value] of Object.entries(links)) {
-    const klass = `bi bi-${key}`;
+    const klass = `bi bi-${value.iconClass}`;
     linkList.push(
-      <Link href={value} className="mx-[0.5em] text-lg lg:text-xl lg:mx-2 text-center" key={key} aria-label={key}>
+      <Link href={value.url} className="mx-[0.5em] text-lg lg:text-xl lg:mx-2 text-center" key={key} aria-label={key}>
         <i className={klass}></i>
       </Link>
     );
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,9 +8,21 @@ export const AUTHOR_PROFILES = {
   description:
     "ギタリスト・歌うたい・コンポーザ・プログラマ・デザイナー。ソフトウェアとハードウェアの狭間を際限なく行き来する自由工作員として奔走する傍ら、Senkawos／タとエ／大島武宜と働く機械などで演奏活動を行なっている。好きな猛禽類はフクロウ。",
   links: {
-    "twitter-x": "https://twitter.com/takenorioshima",
-    facebook: "https://fb.me/takenorioshima",
-    instagram: "https://instagram.com/takenorioshima",
-    github: "https://github.com/takenorioshima",
+    twitter: {
+      iconClass: "twitter-x",
+      url: "https://twitter.com/takenorioshima",
+    },
+    facebook: {
+      iconClass: "facebook",
+      url: "https://fb.me/takenorioshima",
+    },
+    instagram: {
+      iconClass: "instagram",
+      url: "https://instagram.com/takenorioshima",
+    },
+    github: {
+      iconClass: "github",
+      url: "https://github.com/takenorioshima",
+    },
   },
 };
