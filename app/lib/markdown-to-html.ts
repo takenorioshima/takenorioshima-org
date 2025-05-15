391f0b28f1b3641bf7d01d03ad7aa269b8753b22
Takenori Oshima <takenorioshima@gmail.com>
2025-05-15 14:18:52 +0900

    update: Add syntax highlighting

--- app/lib/markdown-to-html.ts
+++ app/lib/markdown-to-html.ts
@@ -2,15 +2,15 @@ import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
-import rehypePrettyCode from "rehype-pretty-code";
+import rehypePrism from "rehype-prism-plus";
+import rehypeCodeTitles from "rehype-code-titles";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypePrettyCode, {
-      // See Options section below.
-    })
+    .use(rehypeCodeTitles)
+    .use(rehypePrism)
     .use(rehypeStringify)
     .process(markdown);
   return String(result);
