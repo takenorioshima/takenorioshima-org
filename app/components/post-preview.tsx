f34bb6b2577e6355a545e544ac2bf3cdecf94582
Takenori Oshima <takenorioshima@gmail.com>
2025-05-15 12:34:00 +0900

    fix: Fix path

--- app/components/post-preview.tsx
+++ app/components/post-preview.tsx
@@ -1,7 +1,7 @@
 import DateFormatter from "./date-formatter";
 import CoverImage from "./cover-image";
 import Link from "next/link";
-import { Post } from "@/app/lib/types";
+import { Post } from "@/lib/types";
 
 type PostPreviewProps = {
   post: Post;
