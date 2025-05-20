f80c7ee25180f376d7cc15649a859fa3e66a9bbb
Takenori Oshima <takenorioshima@gmail.com>
2025-05-20 14:01:02 +0900

    delete: Delete temporary files

--- _src/components/container.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  children?: React.ReactNode;
-};
-
-const Container = ({ children }: Props) => {
-  return <div className="container max-w-(--breakpoint-lg) mx-auto mt-[48px] mb-10 lg:my-16 px-4">{children}</div>;
-};
-
-export default Container;
