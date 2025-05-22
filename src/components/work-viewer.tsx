dff5b67399f1c1999182878ba2aacd051db26f1d
Takenori Oshima <takenorioshima@gmail.com>
2025-05-22 15:10:24 +0900

    update: Add Embla Carousel

--- src/components/work-viewer.tsx
+++ src/components/work-viewer.tsx
@@ -1,4 +1,8 @@
 import Image from "next-export-optimize-images/image";
+import useEmblaCarousel from "embla-carousel-react";
+import { useEffect } from "react";
+import { EmblaOptionsType } from "embla-carousel";
+import { PrevButton, NextButton, usePrevNextButtons } from "./carousel-buttons";
 
 type Props = {
   slug: string;
@@ -6,18 +10,38 @@ type Props = {
 };
 
 export default function WorkViewer({ slug, images }: Props) {
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+
+  const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi);
+
+  useEffect(() => {
+    if (emblaApi) {
+      console.log(emblaApi.slideNodes()); // Access API
+    }
+  }, [emblaApi]);
+
   return (
-    <section className="work-viewer">
-      {images.map((image) => (
-        <div className="embla__slide">
-          <Image
-            src={"/assets/works/" + slug + "/" + image}
-            alt={`Cover Image for ${slug}`}
-            className={"relative! w-full object-contain object-center"}
-            fill
-          />
+    <section className="p-10">
+      <div className="embla" ref={emblaRef}>
+        <div className="embla__container">
+          {images.map((image) => (
+            <div className="embla__slide">
+              <Image
+                src={"/assets/works/" + slug + "/" + image}
+                alt={`Cover Image for ${slug}`}
+                className={"relative! w-full object-contain object-center"}
+                fill
+              />
+            </div>
+          ))}
         </div>
-      ))}
+        <div className="embla__controls">
+          <div className="embla__buttons">
+            <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+            <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/pages/works/[slug].tsx
+++ src/pages/works/[slug].tsx
@@ -22,7 +22,22 @@ export default function Work({ work }: Props) {
   return (
     <Layout>
       <Container>
-        <h1>{work.title}</h1>
+        <div className="py-5 border-b border-slate-300 flex items-center place-content-between">
+          <div>
+            <div className="mb-2">しごと</div>
+            <h1 className="text-4xl md:text-5xl lg:text-5xl font-semibold tracking-tighter leading-tight mb-6 md:text-left">
+              {work.title}
+            </h1>
+          </div>
+          {work.tags && (
+            <div className="text-right text-slate-700 text-sm">
+              <i className="bi bi-tag mr-1"></i>
+              {work.tags.map((tag) => {
+                return tag;
+              })}
+            </div>
+          )}
+        </div>
         {work.images && <WorkViewer slug={work.slug} images={work.images} />}
       </Container>
     </Layout>
